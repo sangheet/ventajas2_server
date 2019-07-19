c9376a125eef216eb7bbc80b21b94750e06e432e
Norbis Magallanes <norbinski@gmail.com>
2019-07-19 15:47:03 -0500

    FirstCommit
    FirstCommit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+
+//alows cross origin request
+app.use(cors());
+
+const URI = 'mongodb+srv://norbis:test123@cluster0-cedhx.mongodb.net/test?retryWrites=true&w=majority'; //Cloud MongoDB
+//const URI = 'mongodb://localhost:27017/test'; // Local MongoDB
+const OPTS = { useNewUrlParser: true };
+mongoose.connect(URI, OPTS, function(err) {
+  if (err) { return console.error('Conection to DB failed');}
+}); 
+mongoose.connection.once('open', () => {
+console.log('conected to database ON');
+});
+
+// mongoose.connect('', { useNewUrlParser: true });
+// mongoose.connection.once('open', () => {
+//     console.log('conected to database ON');
+// });
+
+app.use('/graphql',graphqlHTTP({
+    schema,
+    graphiql: true
+}));
+app.listen(4000, ()=> {
+  console.log("Listening request on port 4000");
+})
